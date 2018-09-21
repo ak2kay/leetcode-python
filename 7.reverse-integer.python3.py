6b391b682bfd6cf13e8a9b84c1aac6dc1e10e942
Bryce Yang <bryceyang@users.noreply.github.com>
2018-09-22 00:28:06 +0800

    update 7.reverse-integer.python3.py

---
+++ 7.reverse-integer.python3.py
@@ -0,0 +1,53 @@
+#
+# [7] Reverse Integer
+#
+# https://leetcode.com/problems/reverse-integer/description/
+#
+# algorithms
+# Easy (24.43%)
+# Total Accepted:    480.9K
+# Total Submissions: 2M
+# Testcase Example:  '123'
+#
+# Given a 32-bit signed integer, reverse digits of an integer.
+# 
+# Example 1:
+# 
+# 
+# Input: 123
+# Output: 321
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: -123
+# Output: -321
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: 120
+# Output: 21
+# 
+# 
+# Note:
+# Assume we are dealing with an environment which could only store integers
+# within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of
+# this problem, assume that your function returns 0 when the reversed integer
+# overflows.
+# 
+#
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        ans = str(x)[::-1] if x >= 0 else '-' + str(x)[:0:-1]
+        ans = int(ans)
+        if ans < - 2 ** 31 or ans > 2 ** 31 - 1:
+            ans = 0
+
+        return ans
