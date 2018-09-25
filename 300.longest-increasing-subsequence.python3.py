5e9974a70285155187d5efecdc99a8fd53a362e6
Bryce Yang <bryceyang@users.noreply.github.com>
2018-09-25 22:37:00 +0800

    update 300.longest-increasing-subsequence.python3.py

--- 300.longest-increasing-subsequence.python3.py
+++ 300.longest-increasing-subsequence.python3.py
@@ -41,16 +41,27 @@ class Solution:
         if length < 2:
             return length
         
-        res = [1 for i in range(length)]
-        i = 1
-        while i < length:
-            temp = nums[i]
-            res[i] = 1
-            for j in range(i-1, -1, -1):
-                if temp > nums[j]:
-                    res[i] = max(res[i], res[j]+1)
-            i += 1
+        max_v = [0 for i in range(length+1)]
 
-        return max(res)
+        max_v[0] = min(nums) - 1
+        max_v[1] = nums[0]
+        lis = [1 for i in range(length)]
 
-
+        cur_max = 1
+        for i in range(1, length):
+            for j in range(cur_max, -1, -1):
+                if nums[i] > max_v[j]:
+                    lis[i] = j + 1
+                    break
+            # 如果更新了当前最大值，那么更新max_v
+            if lis[i] > cur_max:
+                cur_max = lis[i]
+                max_v[cur_max] = nums[i]
+            # 如果没有更新最大值，那么找到可能被i更新的j
+            # 当nums[i] > max_v[j]也就是进入到了上面for循环中的if条件中
+            # 如果进入了条件中，那么如果nums[i]比原来max_v[j+1]小的时候才有
+            # 更新价值；
+            elif nums[i] > max_v[j] and nums[i] < max_v[j+1]:
+                max_v[j+1] = nums[i]
+        
+        return cur_max
