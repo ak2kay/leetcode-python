51601c2987bdc8e1a543b4f2c827c48d9e9b96bc
Bryce Yang <bryceyang@users.noreply.github.com>
2018-10-12 21:41:40 +0800

    update 701.insert-into-a-binary-search-tree.python3.py

---
+++ 701.insert-into-a-binary-search-tree.python3.py
@@ -0,0 +1,86 @@
+#
+# [784] Insert into a Binary Search Tree
+#
+# https://leetcode.com/problems/insert-into-a-binary-search-tree/description/
+#
+# algorithms
+# Medium (59.80%)
+# Total Accepted:    9.9K
+# Total Submissions: 15.2K
+# Testcase Example:  '[4,2,7,1,3]\n5'
+#
+# Given the root node of a binary search tree (BST) and a value to be inserted
+# into the tree, insert the value into the BST. Return the root node of the BST
+# after the insertion. It is guaranteed that the new value does not exist in
+# the original BST.
+# 
+# Note that there may exist multiple valid ways for the insertion, as long as
+# the tree remains a BST after insertion. You can return any of them.
+# 
+# For example, 
+# 
+# 
+# Given the tree:
+# ⁠       4
+# ⁠      / \
+# ⁠     2   7
+# ⁠    / \
+# ⁠   1   3
+# And the value to insert: 5
+# 
+# 
+# You can return this binary search tree:
+# 
+# 
+# ⁠        4
+# ⁠      /   \
+# ⁠     2     7
+# ⁠    / \   /
+# ⁠   1   3 5
+# 
+# 
+# This tree is also valid:
+# 
+# 
+# ⁠        5
+# ⁠      /   \
+# ⁠     2     7
+# ⁠    / \   
+# ⁠   1   3
+# ⁠        \
+# ⁠         4
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root is None:
+            return TreeNode(val)
+        cur_node = root
+        while cur_node:
+            if val < cur_node.val:
+                if not cur_node.left:
+                    cur_node.left = TreeNode(val)
+                    return root
+                else:
+                    cur_node = cur_node.left
+
+            if val > cur_node.val:
+                if not cur_node.right:
+                    cur_node.right = TreeNode(val)
+                    return root
+                else:
+                    cur_node = cur_node.right
+
